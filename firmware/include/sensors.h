10e9aada10c49ef433c5bcae5bf751f5e8809f17
Saurabh vishwakarma <saurvishwakarma040207@gmail.com>
2025-11-21 08:43:55 +0530

    Update sensors.h

--- firmware/include/sensors.h
+++ firmware/include/sensors.h
@@ -1,1 +1,38 @@
+#ifndef SENSORS_H
+#define SENSORS_H
 
+#include <Arduino.h>
+
+// Sensor data structure
+struct SensorData {
+    float heartRate;
+    int steps;
+    float temperature;
+    int movementCount;
+    int interactionCount;
+    unsigned long timestamp;
+};
+
+// Focus analytics structure
+struct FocusData {
+    float focusScore;
+    int distractionCount;
+    float heartRateAvg;
+    float movementScore;
+    unsigned long sessionStart;
+    unsigned long sessionEnd;
+};
+
+// Function declarations
+void initSensors();
+bool readHeartRate(float &heartRate);
+bool readAccelerometer(float &x, float &y, float &z);
+int calculateSteps();
+float calculateTemperature();
+int detectMovement();
+SensorData collectAllSensorData();
+FocusData calculateFocusScore(const SensorData &data);
+void displaySensorData(const SensorData &data);
+bool isSensorHealthy();
+
+#endif
